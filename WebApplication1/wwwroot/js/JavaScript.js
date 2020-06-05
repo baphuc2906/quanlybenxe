2e0236ecd08a2d23522b986907952bf39bc8dd14
baphuc2906 <tangbaphuc2906@gmail.com>
2020-06-05 19:35:53 +0700

    update-search and creat 3 information: bus station, driver and bus.

--- WebApplication1/wwwroot/js/JavaScript.js
+++ WebApplication1/wwwroot/js/JavaScript.js
@@ -1,6 +1,5 @@
-﻿var bttn = document.createElement("button");
-bttn.value = "Change bgcolor";
-bttn.clientWidth = 100;
-bttn.clientHeight = 100;
-bttn.addEventListener("click", () => { document.body.style.backgroundColor = "#ddd"; });
-document.body.appendChild(bttn);
+﻿$(document).ready(fuction()){
+    $("#toggle-button").click(fuction(){
+        $("#toggle").toggle();
+    })
+}
--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿/*$(document).ready(function(){
+    $("#toggle-button").click(function () {
+        $("#toggle").toggleClass(function () {
+            return 'shownangcao';
+        });
+    });
+});*/
+$(document).ready(function () {
+    $("#toggle-button").click(function () {
+        $("#toggle").toggleClass("shownangcao",5000);
+    });
+});
